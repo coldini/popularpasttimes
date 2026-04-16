8d42655457b2acaba9df97cf2c869c8d6d6a838f
colden Jeanmonod <tur26337@temple.edu>
2026-04-16 09:12:31 +0900

    added an image to the homepage

--- webapp/React_CGF/Home.js
+++ webapp/React_CGF/Home.js
@@ -6,7 +6,9 @@ function Home(){
     return React.createElement('div', {className: 'home'},
         React.createElement('div', {id: 'content'},
             React.createElement('h1', null, 'Hello, and Welcome'),
-            React.createElement('p', null, 'Are you wandering around a country wondering what exactly are you going to do with your time? Look no further! With our help we plan to help you figure out what to do with all that free time!')
+            React.createElement('p', null, 'Are you wandering around a country wondering what exactly are you going to do with your time? Look no further! With our help we plan to help you figure out what to do with all that free time!'),
+            React.createElement('img',{ src: 'style/Traveling.jpg', alt: 'Travel Inspiration', style: { width: '600px', margin:'20px auto',  display: 'block'}
+            })
         )
     );
 }
